801369aeb2e968c7b7b41375aea46065ac576aa7
Craig Sebenik <craig5@pobox.com>
2015-07-26 14:22:47 -0700

    added some simple checks
    Added some checks that are not "required" for the assignment,
    but are kinda needed.

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,11 @@
 ## Take a matrix as the only argument/parameter.
 ## Return a list object with the inverse initialized to NULL.
 makeCacheMatrix <- function(x = matrix()) {
+	# Verify the matrix is square.
+	# (Not required for the assignment, but is required to compute an inverse.)
+	if(nrow(x) != ncol(x)) {
+		stop('Not a square matrix.')
+	}
 	# Initialize the inverse to NULL.
 	inverse <- NULL
 	set <- function(y) {
@@ -25,6 +30,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Solve (inverse) a matrix, using a cached version if available.
 cacheSolve <- function(x, ...) {
+	# Verify the correct object type was given.
+	# If you cannot find the names, then it isn't the correct type of object.
+	if(is.null(names(x))) {
+		stop('Not a valid cacheMatrix object.')
+	}
+	# If the length of the names vector is wrong, don't compare. It's wrong.
+	if(length(names(x)) != 4) {
+		stop('Not a valid cacheMatrix object.')
+	}
+	if(! all(names(x) == c('set', 'get', 'setinverse', 'getinverse'))) {
+		stop('Not a valid cacheMatrix object.')
+	}
 	inverse <- x$getinverse()
 	# If the inverse is not null, then use that cached version.
 	if(!is.null(inverse)) {
