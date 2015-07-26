db895eb88c3a386e2a5b12ecae2f8c636108d7c3
Craig Sebenik <craig5@pobox.com>
2015-07-26 13:50:51 -0700

    working assignment 2 solution
    But, not totally optimal.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file has 2 functions:
+##	- makeCacheMatrix: create an object with a cacheable inverse.
+##	- cacheSolve: returns the inverse of a matrix,
+##			using a cached version if present
 
-## Write a short comment describing this function
 
+## Take a matrix as the only argument/parameter.
+## Return a list object with the inverse initialized to NULL.
 makeCacheMatrix <- function(x = matrix()) {
-
+	# Initialize the inverse to NULL.
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) inverse <<- solve
+	getinverse <- function() inverse
+	list(
+		set = set,
+		get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Solve (inverse) a matrix, using a cached version if available.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getinverse()
+	# If the inverse is not null, then use that cached version.
+	if(!is.null(inverse)) {
+		message('Using cached data.')
+		return(inverse)
+	}
+	# If the inverse IS null,
+	# then compute the inverse and store it in the object.
+	# (I.e. "cache" the solution.)
+	data <- x$get()
+	inverse <- solve(data, ...)
+	x$setinverse(inverse)
+	inverse
 }
+#
+# vim: set tabstop=4:
